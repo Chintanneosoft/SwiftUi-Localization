8bbbcf8863d38ff4e4202e9f73ca8a1317dc8b2c
Chintanneosoft <chintan.rajgor@neosoftmail.com>
2023-11-27 15:45:00 +0530

    Initial Commit

---
+++ SwiftUi Localization/SwiftUi_LocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_LocalizationApp.swift
+//  SwiftUi Localization
+//
+//  Created by Neosoft on 27/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_LocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
